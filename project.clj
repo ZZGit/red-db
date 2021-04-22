a8592058994e7178e99f15162f112c2a21d20141
strongfish <569721086@qq.com>
2021-04-22 10:28:50 +0800

    deploy config

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [conman "0.9.1"]
                  [honeysql "1.0.461"]
